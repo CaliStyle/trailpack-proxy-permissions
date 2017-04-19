ba62a436927c80b28ceaf2a24bd2017f32a1f902
scott-wyatt <scottwyatt86@gmail.com>
2017-04-19 13:24:37 -0400

    Fix User Controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -58,7 +58,7 @@ module.exports = class UserController extends Controller {
           as: 'passports'
         },
         {
-          model: this.app.orm['Roles'],
+          model: this.app.orm['Role'],
           as: 'roles'
         }
       ]
